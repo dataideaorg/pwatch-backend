8551da3c7a93510342649d07295bd6148b043f03
jumashafara <jumashafara0@gmail.com>
2025-12-11 22:04:40 +0300

    feat: added explainers

--- resources/admin.py
+++ resources/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import Explainers, Report, PartnerPublication, Statement
 
-# Register your models here.
+
+@admin.register(Explainers)
+class ExplainersAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'file', 'created_at']
+    search_fields = ['name', 'description']
+    ordering = ['-created_at']
+
+    fieldsets = (
+        ('Explainer Information', {
+            'fields': ('name', 'description', 'file')
+        }),
+    )
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'file', 'created_at']
+    search_fields = ['name', 'description']
+    ordering = ['-created_at']
+
+    fieldsets = (
+        ('Report Information', {
+            'fields': ('name', 'description', 'file')
+        }),
+    )
+
+
+@admin.register(PartnerPublication)
+class PartnerPublicationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'file', 'created_at']
+    search_fields = ['name', 'description']
+    ordering = ['-created_at']
+
+    fieldsets = (
+        ('Partner Publication Information', {
+            'fields': ('name', 'description', 'file')
+        }),
+    )
+
+
+@admin.register(Statement)
+class StatementAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'file', 'created_at']
+    search_fields = ['name', 'description']
+    ordering = ['-created_at']
+
+    fieldsets = (
+        ('Statement Information', {
+            'fields': ('name', 'description', 'file')
+        }),
+    )
--- resources/views.py
+++ resources/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Explainers, Report, PartnerPublication, Statement
+from .serializers import ExplainersSerializer, ReportSerializer, PartnerPublicationSerializer, StatementSerializer
 
-# Create your views here.
+
+class ResourcePagination(PageNumberPagination):
+    page_size = 15
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class ExplainersViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for Explainers
+
+    Provides educational explainer documents
+    """
+    queryset = Explainers.objects.all()
+    serializer_class = ExplainersSerializer
+    pagination_class = ResourcePagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description']
+    ordering_fields = ['created_at', 'name']
+    ordering = ['-created_at']
+
+
+class ReportViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for Reports & Briefs
+
+    Provides reports and brief documents
+    """
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
+    pagination_class = ResourcePagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description']
+    ordering_fields = ['created_at', 'name']
+    ordering = ['-created_at']
+
+
+class PartnerPublicationViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for Partner Publications
+
+    Provides partner publication documents
+    """
+    queryset = PartnerPublication.objects.all()
+    serializer_class = PartnerPublicationSerializer
+    pagination_class = ResourcePagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description']
+    ordering_fields = ['created_at', 'name']
+    ordering = ['-created_at']
+
+
+class StatementViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for Statements
+
+    Provides official statement documents
+    """
+    queryset = Statement.objects.all()
+    serializer_class = StatementSerializer
+    pagination_class = ResourcePagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description']
+    ordering_fields = ['created_at', 'name']
+    ordering = ['-created_at']
