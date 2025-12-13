45a15c2c38e0a2fb2eb4a1d7ac6b68be72b587dd
jumashafara <jumashafara0@gmail.com>
2025-12-13 10:33:08 +0300

    fix: updated static files dirs

--- main/settings.py
+++ main/settings.py
@@ -134,7 +134,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'main' / 'static',
+]
 # Media files configuration
 MEDIA_URL = '/media/'
 
